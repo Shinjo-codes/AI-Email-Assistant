bf230f07a6c7c0065ed1615e28dc91c41942b095
Shinjo-codes <ojoshinajohnson@gmail.com>
2025-01-24 06:24:26 -0800

    Email Response Assistant

---
+++ src/main/java/com/email/email_writer_sb/app/EmailGeneratorController.java
@@ -0,0 +1,27 @@
+package com.email.email_writer_sb.app;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/email")
+@AllArgsConstructor
+public class EmailGeneratorController {
+
+    private final EmailGeneratorService emailGeneratorService;
+
+    @PostMapping("/generate")
+    public ResponseEntity<String> generatedEmail(@RequestBody EmailRequest emailRequest) {
+        String response = emailGeneratorService.generateEmailReply(emailRequest);
+
+        return ResponseEntity.ok(response);
+
+    }
+
+
+
+}
